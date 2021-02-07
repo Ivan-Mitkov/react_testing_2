90c638bc7e650837442d7faa0cd750eca69654f0
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-07 10:45:18 +0200

    Redux props App

--- jotto_redux/src/App.js
+++ jotto_redux/src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./App.css";
 import Congrats from "./Congrats";
 import GuessedWord from "./GuessWord";
 import Input from "./Input";
+import { guessword } from "./actions/index";
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Congrats success={false} />
-        <GuessedWord
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
+        <Congrats success={this.props.success} />
+        <GuessedWord guessedWords={this.props.guessedWords} />
         <Input />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ success, guessedWords, secretWord }, ownProps) => {
+  return {
+    success,
+    guessedWords,
+    secretWord,
+  };
+};
+export default connect(mapStateToProps, { guessword })(App);
--- jotto_redux/src/App.test.js
+++ jotto_redux/src/App.test.js
@@ -1,4 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { shallow } from "enzyme";
+import { storeFactory } from "./test/testUtils";
 import App from "./App";
 
-test("renders learn react link", () => {});
+const setUp = (state = {}) => {
+  const store = storeFactory(state);
+  const wraper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+  return wraper;
+};
+
+describe("redux properties", () => {
+  test("should have access to success state ", () => {
+    const success = true;
+    const wrapper = setUp({ success });
+    //create react component instance get props an than success prop
+    const successProps = wrapper.instance().props.success;
+    expect(successProps).toBe(success);
+  });
+
+  test("should have access to secret word", () => {
+    const secretWord = "party";
+    const wrapper = setUp({ secretWord });
+    //create react component instance get props an than success prop
+    const secretWordProps = wrapper.instance().props.secretWord;
+    expect(secretWordProps).toBe(secretWord);
+  });
+  test("should have access to guessedWord state", () => {
+    const guessedWords = [{ guessedWord: "train", letterCountMatch: 3 }];
+    const wrapper = setUp({ guessedWords });
+    //create react component instance get props an than success prop
+    const guessedWordsProps = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProps).toBe(guessedWords);
+  });
+  test("guessWord action creator is a function prop ", () => {
+    const wrapper = setUp();
+    const guessWordProps = wrapper.instance().props.guessword;
+    expect(guessWordProps).toBeInstanceOf(Function);
+  });
+});
