f8f0b4739dc244af2721fc11fded8e665d96955b
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-13 16:57:19 +0200

    test useEffect

--- jotto_hooks/src/App.js
+++ jotto_hooks/src/App.js
@@ -2,9 +2,31 @@ import React from "react";
 import "./App.css";
 import Congrats from "./Congrats";
 import GuessedWord from "./GuessWord";
+import hookActions from "./actions/hookActions";
+
+export const SET_SECRET_WORD = "SET_SECRET_WORD";
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_SECRET_WORD:
+      return {
+        ...state,
+        secretWord: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 const App = () => {
+  const initialState = { secretWord: null };
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: SET_SECRET_WORD, payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
   return (
-    <div className="container" data-test='comp-app'>
+    <div className="container" data-test="comp-app">
       <Congrats success={true} />
       <GuessedWord
         guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
--- jotto_hooks/src/App.test.js
+++ jotto_hooks/src/App.test.js
@@ -1,13 +1,25 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttribute } from "./test/testUtils";
 import App from "./App";
+import hookActions, { getSecretWord } from "./actions/hookActions";
 
+const mockGetSecretWord = jest.fn();
 const setup = () => {
-  return shallow(<App />);
+  //clear before every test
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+  return mount(<App />);
 };
 
 test("App should render without error", () => {
   const wrapper = setup();
   const component = findByTestAttribute(wrapper, "comp-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on app mount ", () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
