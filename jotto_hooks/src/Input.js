e59f34225bfdaccf98acf02613146d7d8debd8b0
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-13 14:09:56 +0200

    state update with value from input box

--- jotto_hooks/src/Input.js
+++ jotto_hooks/src/Input.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 const Input = ({ secretWord = "party" }) => {
   const [currentGuess, setCurrentGuess] = React.useState("");
-  
+
   return (
     <div className="container" data-test="comp-input">
       <form className="form-inline">
@@ -11,6 +11,7 @@ const Input = ({ secretWord = "party" }) => {
           className="mb-2 mx-sm-3"
           type="text"
           placeholder="enter guess"
+          onChange={(e) => setCurrentGuess(e.target.value)}
         />
         <button data-test="submit-button" className="btn btn-primary mb-2">
           Submit
--- jotto_hooks/src/Input.test.js
+++ jotto_hooks/src/Input.test.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { shallow } from "enzyme";
 import { findByTestAttribute, checkProps } from "./test/testUtils";
 import Input from "./Input";
@@ -18,4 +19,20 @@ test("Input should render without error", () => {
 test("does not throw warning with expected props ", () => {
   const expectedProps = { secretWord: "party" };
   checkProps(Input, expectedProps);
+});
+
+describe("state controlled input field", () => {
+  test("state update with value from input box ", () => {
+    //use state return an array with this fn as a second function
+    const mockCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockCurrentGuess]);
+    const wrapper = setup();
+    const inputBox = findByTestAttribute(wrapper, "input-box");
+    //simulate input box getting a value train
+    //apply mock event to change event
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockCurrentGuess).toHaveBeenCalledWith("train");
+  });
 });
