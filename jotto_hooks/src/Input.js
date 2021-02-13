74dabf104822caa3ddc18b1777fc35760edfdd37
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-13 13:01:48 +0200

    input component

--- jotto_hooks/src/Input.js
+++ jotto_hooks/src/Input.js
@@ -1,7 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 const Input = ({ secretWord = "party" }) => {
-  return <div className="container" data-test="comp-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  
+  return (
+    <div className="container" data-test="comp-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button data-test="submit-button" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
